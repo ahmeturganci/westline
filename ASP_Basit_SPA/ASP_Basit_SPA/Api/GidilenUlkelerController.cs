53157f40f97fccf868a38d0b65a34e9a3d63908b
ertugrulungor <ertugrulungor@gmail.com>
2016-09-22 09:34:07 +0300

    Ek api sınıfları eklendi

---
+++ ASP_Basit_SPA/ASP_Basit_SPA/Api/GidilenUlkelerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASP_Basit_SPA.Api
+{
+    public class GidilenUlkelerController : ApiController
+    {
+        Westline context = new Westline();
+        // GET api/<controller>
+        public IEnumerable<GidilenUlkeler> Get()
+        {
+            return context.GidilenUlkelers;
+        }
+
+        // GET api/<controller>/5
+        public GidilenUlkeler Get(int id)
+        {
+            return context.GidilenUlkelers.FirstOrDefault(x=>x.Id==id);
+        }
+
+        // POST api/<controller>
+        public int Post(int kisiId,int ulkeId)
+        {
+            GidilenUlkeler gu = new GidilenUlkeler();
+            gu.KisiId = kisiId;
+            gu.UlkeId = ulkeId;
+
+            context.GidilenUlkelers.Add(gu);
+            context.SaveChanges();
+            return gu.Id;
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, int kisiId, int ulkeId)
+        {
+            GidilenUlkeler gu = context.GidilenUlkelers.FirstOrDefault(x=>x.Id==id);
+            gu.KisiId = kisiId;
+            gu.UlkeId = ulkeId;
+
+            context.GidilenUlkelers.Add(gu);
+            context.SaveChanges();
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            GidilenUlkeler gu = context.GidilenUlkelers.FirstOrDefault(x => x.Id == id);
+            context.GidilenUlkelers.Remove(gu);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ASP_Basit_SPA/ASP_Basit_SPA/Api/KisiReferansController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASP_Basit_SPA.Api
+{
+    public class KisiReferansController : ApiController
+    {
+        Westline context = new Westline();
+        // GET api/<controller>
+        public IEnumerable<KisiReferan> Get()
+        {
+            return context.KisiReferans;
+        }
+
+        // GET api/<controller>/5
+        public KisiReferan Get(int id)
+        {
+            return context.KisiReferans.FirstOrDefault(x=>x.Id==id);
+        }
+
+        // POST api/<controller>
+        public int Post(int kisiId, int refId)
+        {
+            KisiReferan kr = new KisiReferan();
+            kr.KisiId = kisiId;
+            kr.ReferansId = refId;
+
+            context.KisiReferans.Add(kr);
+            context.SaveChanges();
+            return kr.Id;
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, int kisiId, int refId)
+        {
+            KisiReferan kr = context.KisiReferans.FirstOrDefault(x=>x.Id==id);
+            kr.KisiId = kisiId;
+            kr.ReferansId = refId;
+
+            context.KisiReferans.Add(kr);
+            context.SaveChanges();
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            KisiReferan kr = context.KisiReferans.FirstOrDefault(x => x.Id == id);
+            context.KisiReferans.Remove(kr);
+            context.SaveChanges();
+        }
+    }
+}
