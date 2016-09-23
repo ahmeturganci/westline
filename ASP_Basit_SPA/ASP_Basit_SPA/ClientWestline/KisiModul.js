09ae7f280f8422a477187fd3e9f17412e9011b2f
Ahmet Urgancı <aahmet.272@gmail.com>
2016-09-23 12:50:36 +0300

    ufaktan ilk sayfaya başlandı(şu an karışık ve hatalı)

---
+++ ASP_Basit_SPA/ASP_Basit_SPA/ClientWestline/KisiModul.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var app = angular.module("KisiModul", "ngRoute");
+
+    var config = (function ($routeProvider) {
+        $routeProvider
+         .when("/kisitanima", {
+             templateUrl: "/ClientWestline/Views/kisitanima.html"
+         })
+         .otherwise({
+             redirectTo: "/list"
+         });
+    });
+})
