b9a87d7a9b84b295741ea6ae61032496da791054
AhmetU <aahmet.272@gmail.com>
2016-09-24 18:49:54 +0300

    Hatalı kısım düzeltilmeye çalışıldı.(hala hatalı :( )

--- ASP_Basit_SPA/ASP_Basit_SPA/ClientWestline/KisiTanimaController.js
+++ ASP_Basit_SPA/ASP_Basit_SPA/ClientWestline/KisiTanimaController.js
@@ -2,16 +2,32 @@
     //ekleme işi 
     var KisiTanimaController= function ($scope, $http) {
         $scope.kaydet = function () {
-         
+
             //api
-            $http.post("http://localhost:11226/Api/Kisi?tcKimlikNo=" +$scope.tc+ "&ad="+$scope.ad+"&ortaAd="+$scope.ortaAd+"&soyad="+$scope.soyad+"&email="+$scope.email+"&tel="+$scope.tel).
-                success(function (data) {
-                    console.log("başarılı");
-                }).error(function (data) {
-                    console.log("hata");
-                });
-            
+            /* $http.post("http://localhost:11226/Api/Kisi?tcKimlikNo=" +$scope.tc+ "&ad="+$scope.ad+"&ortaAd="+$scope.ortaAd+"&soyad="+$scope.soyad+"&email="+$scope.email+"&tel="+$scope.tel).
+                 success(function (data) {
+                     console.log("başarılı");
+                 }).error(function (data) {
+                     console.log("hata");
+                 });
+             
+         }*/
+            $http({
+                url: 'http://localhost:11226/Api/Kisi?tcKimlikNo=' +$scope.tc+ "&ad="+$scope.ad+"&ortaAd="+$scope.ortaAd+"&soyad="+$scope.soyad+"&email="+$scope.email+"&tel="+$scope.tel,
+                dataType: "json",
+                method: "POST",
+                data: JSON.stringify({ "foo": "bar" }),
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                }
+            }).success(function (response) {
+                $scope.response = response;
+            }).error(function (error) {
+                $scope.error = error;
+            });
         }
+
+
 
     }
   app.controller("KisiTanimaController", KisiTanimaController);
