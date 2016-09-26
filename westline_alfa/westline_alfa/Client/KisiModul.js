1d67b3d41f6d991ce59bf155e2fb6c3532117faf
AhmetU <aahmet.272@gmail.com>
2016-09-26 14:19:00 +0300

    Angular sınıfları ve ekran oluşturuldu

---
+++ westline_alfa/westline_alfa/Client/KisiModul.js
@@ -0,0 +1,17 @@
+﻿(function () {
+
+    var app = angular.module('KisiModul', ["ngRoute"]);
+    var config = (function ($routeProvider) {
+        $routeProvider
+         .when("/BirinciSayfa", {
+             templateUrl: "/Client/Views/BirinciSayfa.html"
+         })
+         .otherwise({
+             redirectTo: "/BirinciSayfa"
+         });
+    });
+
+
+    
+    app.config(config);
+}())
---
+++ westline_alfa/westline_alfa/Client/KisiTanimaController.js
@@ -0,0 +1,13 @@
+﻿(function (app) {
+       var KisiTanimaController = function ($scope, $http) {
+        $scope.kaydet = function () {
+            $http.post("Birinci/KisiEkle?ad=" + $scope.ad + "&email=" + $scope.email).
+                success(function (data) {
+                    console.log(data);
+                }).error(function (data) {
+                    console.log(data);
+                });
+        }
+    }
+    app.controller("KisiTanimaController", KisiTanimaController);
+}(angular.module("KisiModul")))
