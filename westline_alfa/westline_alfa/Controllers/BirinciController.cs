8559a5a09e59175bdbe9ee572741f76b4acdacd2
ertugrulungor <ertugrulungor@gmail.com>
2016-09-26 13:59:46 +0300

    Controller ve helper sınıfı

---
+++ westline_alfa/westline_alfa/Controllers/BirinciController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace westline_alfa.Controllers
+{
+    public class BirinciController : Controller
+    {
+        public int KisiEkle(string tc = "", string ad = "", string ortaAd="", string soyad = "", string email = "", string tel = "")
+        {
+            helper.helper h = new helper.helper();
+            if(h.FormKontrol(tc, ad, soyad, email, tel) == true)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ westline_alfa/westline_alfa/helper/helper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace westline_alfa.helper
+{
+    public class helper
+    {
+        public bool FormKontrol(params object[] elemanlar)
+        {
+            bool kontrol = true;
+            foreach (object i in elemanlar)
+            {
+                if (i is string )
+                {
+                    if ((string)i == "")
+                    {
+                        kontrol = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    if((int)i == -1)
+                    {
+                        kontrol = false;
+                        break;
+                    }
+                }
+            }
+
+            return kontrol;
+        } 
+    }
+}
