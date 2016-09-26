1d2d0ec0ba07c84544797951878071e320ed7036
ertugrulungor <ertugrulungor@gmail.com>
2016-09-26 14:33:09 +0300

    komit

--- westline_alfa/westline_alfa/Client/KisiTanimaController.js
+++ westline_alfa/westline_alfa/Client/KisiTanimaController.js
@@ -1,7 +1,7 @@
 ﻿(function (app) {
        var KisiTanimaController = function ($scope, $http) {
         $scope.kaydet = function () {
-            $http.post("Birinci/KisiEkle?ad=" + $scope.ad + "&email=" + $scope.email).
+            $http.post("Birinci/KisiEkle?tc=" +$scope.tc+ "&ad=" + $scope.ad + "&ortaAd=" +$scope.ortaAd+ "&soyad="+$scope.soyad+"&email=" + $scope.email + "&tel="+$scope.tel).
                 success(function (data) {
                     console.log(data);
                 }).error(function (data) {
--- westline_alfa/westline_alfa/Controllers/BirinciController.cs
+++ westline_alfa/westline_alfa/Controllers/BirinciController.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using westline_alfa.Models;
 
 namespace westline_alfa.Controllers
 {
     public class BirinciController : Controller
     {
+        westlineDB db = new westlineDB();
         public int KisiEkle(string tc = "", string ad = "", string ortaAd="", string soyad = "", string email = "", string tel = "")
         {
             helper.helper h = new helper.helper();
-            if(h.FormKontrol(tc, ad, soyad, email, tel) == true)
+            if(h.FormKontrol(tc, ad, soyad, email, tel))
             {
-                return 1;
+                Kisi k = new Kisi();
+                k.TcKimlikNo = tc;
+                k.Ad = ad;
+                k.OrtaAd = ortaAd;
+                k.Soyad = soyad;
+                
+
+                Iletisim i = new Iletisim();
+                
             }
             else
             {
