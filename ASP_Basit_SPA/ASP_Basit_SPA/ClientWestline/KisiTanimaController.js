fc34abfafb0032b888023fc4004f5f2200763d59
AhmetU <aahmet.272@gmail.com>
2016-09-26 13:31:28 +0300

    Controller Apiden arındırıldı

--- ASP_Basit_SPA/ASP_Basit_SPA/ClientWestline/KisiTanimaController.js
+++ ASP_Basit_SPA/ASP_Basit_SPA/ClientWestline/KisiTanimaController.js
@@ -3,6 +3,12 @@
     var KisiTanimaController= function ($scope, $http) {
         $scope.kaydet = function () {
 
+            $http.post("Kisi/Post?ad=" + $scope.ad + "&email=" + $scope.email).
+                success(function (data) {
+                    console.log("başarılı");
+                }).error(function (data) {
+                    console.log("hata");
+                });
             //api
             /* $http.post("http://localhost:11226/Api/Kisi?tcKimlikNo=" +$scope.tc+ "&ad="+$scope.ad+"&ortaAd="+$scope.ortaAd+"&soyad="+$scope.soyad+"&email="+$scope.email+"&tel="+$scope.tel).
                  success(function (data) {
