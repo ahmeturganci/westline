a13eb843d9938f8c06a15d2aafcc80c2e79c6645
ertugrulungor <ertugrulungor@gmail.com>
2016-09-27 13:52:37 +0300

    Sayfa geçiş ok, çalışıyor

--- westline_alfa/westline_alfa/Client/KisiTanimaController.js
+++ westline_alfa/westline_alfa/Client/KisiTanimaController.js
@@ -2,15 +2,13 @@
     var KisiTanimaController = function ($scope, $http,$location,$window) {
         
         $scope.IkinciSayfa = function () {
-            
             $http.post("/Birinci/KisiEkle?tc=" + $scope.tc + "&ad=" + $scope.ad + "&ortaAd=" + $scope.ortaAd + "&soyad=" + $scope.soyad + "&email=" + $scope.email + "&tel=" + $scope.tel).
                 
                 success(function (data) {
-                    console.log(data);
-                    /*
+                    console.log(data.basari);
                     if (data.basari == 1) {
                         $window.location.href = '#/IkinciSayfa';
-                    }*/
+                    }
                     
                 }).error(function (data) {
                     console.log(data);
