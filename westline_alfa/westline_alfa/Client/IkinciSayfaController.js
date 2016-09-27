6ce5cb3b4e71a8a72c9b223f9490b4db8f68ffd7
AhmetU <aahmet.272@gmail.com>
2016-09-28 02:17:23 +0300

    Post kısımları eklendi (test edilmedi)

--- westline_alfa/westline_alfa/Client/IkinciSayfaController.js
+++ westline_alfa/westline_alfa/Client/IkinciSayfaController.js
@@ -1,7 +1,7 @@
 ﻿(function (app) {
     var IkinciSayfaController = function ($scope, $http) {
         $scope.IkinciSayfa = function () {
-            $http.post("").
+            $http.post("/Birinci/KisiDetayEkle?babaAd=" + $scope.babaAd + "&anneAd=" + $scope.anneAd + "&ingilizceSeviye=" + $scope.ingilizceSeviye + "&pasaport=" + $scope.pasaport + "&dogumTarih=" + $scope.dogumTarih + "&skype=" + $scope.skype + "&adresIkinciSatir=" + $scope.adresIkinciSatir + "&sehir=" + $scope.sehir + "&eyalet=" + $scope.eyalet + "&postaKod=" + $scope.postaKod + "&ulkeId=" + $scope.ulkeId + "&acilAd=" + $scope.acilAd + "&acilSoyad=" + $scope.acilSoyad + "&acilTel=" + $scope.acilTel)
                 success(function (data) {
                     console.log(data.basari);
                     if (data.basari == 1) {
--- westline_alfa/westline_alfa/Client/KisiTanimaController.js
+++ westline_alfa/westline_alfa/Client/KisiTanimaController.js
@@ -2,7 +2,7 @@
     var KisiTanimaController = function ($scope, $http,$location,$window) {
         
         $scope.BirinciSayfa = function () {
-            $http.post("/Birinci/KisiEkle?tc=" + $scope.tc + "&ad=" + $scope.ad + "&ortaAd=" + $scope.ortaAd + "&soyad=" + $scope.soyad + "&email=" + $scope.email + "&tel=" + $scope.tel).
+            $http.post("/Birinci/EgitimEkle?tc=" + $scope.tc + "&liseAd=" + $scope.liseAd + "&baslangic=" + $scope.baslangic + "&bitis=" + $scope.bitis + "&alan=" + $scope.alan + "&liseTamAdres=" + $scope.liseTamAdres + "&liseTamAdresIkinciSatir=" + $scope.liseTamAdresIkinciSatir + "&liseSehir=" + $scope.liseSehir + "&liseEyalet=" + $scope.liseEyalet + "&lisePostaKod=" + $scope.lisePostaKod + "&liseUlkeId=" + $scope.liseUlkeId + "&uniAd=" + $scope.uniAd + "&sinif=" + $scope.sinif + "&bolum=" + $scope.bolum + "&okulNo=" + $scope.okulNo + "&acilisT=" + $scope.acilisT + "&kapanisT=" + $scope.kapanisT + "&uniTamAdres=" + $scope.uniTamAdres + "&uniTamAdresIki=" + $scope.uniTamAdresIki + "&uniSehir=" + $scope.uniSehir + "&uniEyalet=" + $scope.uniEyalet + "&uniPostaKod=" + $scope.uniPpstaKod + "&uniUlkeId=" + $scope.uniUlkeId).
                 
                 success(function (data) {
                     console.log(data.basari);
