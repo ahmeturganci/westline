d24e0d9c8371951a3af49168ed570d5eedebe998
ertugrulungor <ertugrulungor@gmail.com>
2016-09-29 12:40:55 +0300

    dorduncucontroller

--- westline_alfa/westline_alfa/Controllers/DorduncuController.cs
+++ westline_alfa/westline_alfa/Controllers/DorduncuController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +9,15 @@ namespace westline_alfa.Controllers
 {
     public class DorduncuController : Controller
     {
-        //public JsonResult BelgeEkle()
-        //{
-
-        //}
+        public int BelgeEkle(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Images/"), fileName);
+                file.SaveAs(path);
+            }
+            return 0;
+        }
     }
 }
