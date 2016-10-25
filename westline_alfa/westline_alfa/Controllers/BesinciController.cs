f3f0b8b398867d6582ef7f85cdaae5e24cf5d51b
ertugrulungor <ertugrulungor@gmail.com>
2016-10-26 00:35:10 +0300

    Beşinci sayfa dinamikleştirildi. Güncelleştirme ve veri kayıt işlemleri yapıldı

--- westline_alfa/westline_alfa/Controllers/BesinciController.cs
+++ westline_alfa/westline_alfa/Controllers/BesinciController.cs
@@ -13,41 +13,29 @@ namespace westline_alfa.Controllers
 
         westlineDB db = new westlineDB();
         helper.helper h = new helper.helper();
-
-        public JsonResult BesinciSayfa(int dogumYeri = -1, string vatandasUlke = "", int vatandasNo = -1, int ikiVatandasNo = -1, string AbdSsn = "", int amerikadaBulunduMu=-1, string amerikadaBulunmaTarih = "", int amerikaBulunduguSure=0, int oncedenAmerikaVizesi = -1, int oncedenAmerikaVizeRet = -1, string oncedenAmerikaVizeRetNedeni = "", int amerikaVatandasGocmenBasvuru=-1, string babaDogumTarihi = "", int babaAmerikadaMi = -1, int askerlikYapti = -1, int sonBesYil = -1, string anneDogumTarihi = "", int anneAmerikadaMi = -1,string amerikaAkrabaBilgi=""){
-
-            
-                Kisi k = db.Kisis.Find(Session["id"]);
-                ds160 ds = new ds160();
-                ds.AbdSsn = AbdSsn;
-                ds.AmerikaAkrabaBilgi = amerikaAkrabaBilgi;
-                ds.AmerikaBulunduguSure = amerikaBulunduguSure.ToString();
-                ds.AmerikaBulunduguTarih = Convert.ToDateTime(amerikadaBulunmaTarih);
-                ds.AmerikadaBulunduMu = amerikadaBulunduMu == 1 ? true : false; ;
-                ds.AmerikaVatandasGocmenBasvuru= amerikaVatandasGocmenBasvuru == 1 ? true : false;
-                ds.AnneAmerikadaMi= anneAmerikadaMi == 1 ? true : false;
-                ds.BabaAmerikadaMi= babaAmerikadaMi == 1 ? true : false;
-                ds.AnneDogumTarihi = Convert.ToDateTime(anneDogumTarihi);
-                ds.BabaDogumTarihi = Convert.ToDateTime(babaDogumTarihi);
-                ds.DogumYeri = dogumYeri;
-                ds.VatandasNo = vatandasNo.ToString();
-                ds.IkinciUlkeVatandasNo = ikiVatandasNo.ToString();
-                ds.OncedenAmerikaVizesiAldı = oncedenAmerikaVizesi == 1 ? true : false;
-                ds.OncedenAmerikaVizeRet = oncedenAmerikaVizeRet == 1 ? true : false;
-                ds.OncedenAmerikaVizeRetNedeni = oncedenAmerikaVizeRetNedeni;
-                ds.AmerikaVatandasGocmenBasvuru = amerikaVatandasGocmenBasvuru == 1 ? true : false;
-                //eksik olabilir.
-                db.ds160.Add(ds);
-                db.SaveChanges();
-
-
-                var jsonModel = new
+        public JsonResult dsEkle()
+        {
+            var jsonResult = (object)null;
+            if (h.VeriEkle(Request.QueryString))
+            {
+                jsonResult = new
                 {
                     basari = 1
                 };
-                return Json(jsonModel, JsonRequestBehavior.AllowGet);
-            
+            }
+            else
+            {
+                jsonResult = new
+                {
+                    basari = 0
+                };
+            }
+            return Json(jsonResult, JsonRequestBehavior.AllowGet);
+        }
 
+        public JsonResult elemans(int sayfa, int kisiId)
+        {
+            return h.ElemanCek(sayfa, kisiId);
         }
     }
 }
